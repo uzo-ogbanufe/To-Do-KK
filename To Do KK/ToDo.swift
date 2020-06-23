1e4c24d7a39f8db484e7a69f9506eb2da9a21ce1
Uzo Ogbanufe <uzoey101@gmail.com>
2020-06-23 13:35:27 -0500

    Initial Commit

---
+++ To Do KK/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  To Do KK
+//
+//  Created by Uzo Ogbanufe on 6/22/20.
+//  Copyright © 2020 Uzo Ogbanufe. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+    }
